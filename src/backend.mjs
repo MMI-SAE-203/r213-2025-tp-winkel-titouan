0d610cec5d3797de1fd5275906612ca4a2fc7f45
winkel-titouan <titouan.winkel@edu.univ-fcomte.fr>
2025-01-31 09:00:46 +0100

    commit 1

---
+++ src/backend.mjs
@@ -0,0 +1,25 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase("http ://127.0.0.1 :8090");
+
+import PocketBase from "pocketbase";
+
+const db = new PocketBase("http://127.0.0.1:8090");
+
+export async function getOffres() {
+    try {
+        let data = await db.collection("Maison").getFullList({
+            sort: "-created",
+        });
+        data = data.map((maison) => {
+            maison.image_url = db.files.getURL(maison, maison.photo_maison);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log(
+            "Une erreur est survenue en lisant la liste des maisons",
+            error,
+        );
+        return [];
+    }
+}
